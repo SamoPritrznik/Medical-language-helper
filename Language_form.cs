6a9e47f704db45fddd4e061c570f54160a8202c0
Samo Pritržnik Pungaršek <50667049+SamoPritrznik@users.noreply.github.com>
2020-04-07 12:56:02 +0200

    Finished the languages window.

--- Language_form.cs
+++ Language_form.cs
@@ -19,24 +19,36 @@ namespace medical_project_real
         public Language_form()
         {
             InitializeComponent();
-            
+            Combobx_lang.Items.AddRange(dat.get_languages().ToArray());
         }
 
         private void Btn_save_new_Click(object sender, EventArgs e)
         {
-            dat.open();
             dat.save_language(txtbx_mother_new.Text, txtbx_foreign_new.Text);
-            dat.close();
+            Combobx_lang.Items.Clear();
+            Combobx_lang.Items.AddRange(dat.get_languages().ToArray());
         }
 
         private void Btn_save_old_Click(object sender, EventArgs e)
         {
 
+            int id = dat.get_language_id(Combobx_lang.SelectedItem.ToString());
+            dat.change_languages(txtbx_foreign_old.Text, txtbx_mother_old.Text, id);
+            Combobx_lang.Items.Clear();
+            Combobx_lang.Items.AddRange(dat.get_languages().ToArray());
+            txtbx_foreign_old.Clear();
+            txtbx_mother_old.Clear();
         }
 
         private void Btn_delete_Click(object sender, EventArgs e)
         {
-
+            int id = dat.get_language_id(Combobx_lang.SelectedItem.ToString());
+            dat.delete_languages(id);
+            Combobx_lang.Items.Clear();
+            Combobx_lang.Items.AddRange(dat.get_languages().ToArray());
+            txtbx_foreign_old.Clear();
+            txtbx_mother_old.Clear();
+            
         }
 
         private void Btn_back_Click(object sender, EventArgs e)
--- database.cs
+++ database.cs
@@ -10,32 +10,118 @@ namespace medical_project_real
 {
     public class databases
     {
-        public SQLiteConnection con = new SQLiteConnection("data source= questionsAnswers.db");
-        public SQLiteCommand comm;
-
-        public void open()
-        {
-            con.Open();
-            comm = new SQLiteCommand(con);
-        }
-
-        public void close()
-        {
-            comm.Dispose();
-            con.Close();
-        }
+        
 
         // Languages
         public void save_language(string mother_name, string foreign_name)
         {
+            
             try
             {
-                comm.CommandText = "INSERT INTO languages(Name_Eng, Name_Native) VALUES (@1, @2);";
+                SQLiteConnection con = new SQLiteConnection("data source= questionsAnswers.db");
+                con.Open();
+                SQLiteCommand comm = new SQLiteCommand(con);
+                comm.CommandText = "INSERT INTO Languages(Name_Eng, Name_Native) VALUES (@1, @2);";
                 comm.Parameters.AddWithValue("@1", foreign_name);
                 comm.Parameters.AddWithValue("@2", mother_name);
                 comm.ExecuteNonQuery();
+                comm.Dispose();
+                con.Close();
             }
             catch(SQLiteException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            
+        }
+
+        public List<String> get_languages()
+        {
+            List<String> listek = new List<String>();
+            try
+            {
+                int i = 0;
+                SQLiteConnection con = new SQLiteConnection("data source= questionsAnswers.db");
+                con.Open();
+                SQLiteCommand comm = new SQLiteCommand(con);
+                comm.CommandText = "SELECT Name_Eng FROM languages;";
+                SQLiteDataReader rd = comm.ExecuteReader();
+                
+                while(rd.Read())
+                {
+                    listek.Add(rd.GetString(0).ToString());
+                }
+                comm.Dispose();
+                con.Close();
+            }
+            catch(SQLiteException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            return listek;
+        }
+
+        public int get_language_id(string xname)
+        {
+            int id = 0;
+            try
+            {
+                SQLiteConnection con = new SQLiteConnection("data source= questionsAnswers.db");
+                con.Open();
+                SQLiteCommand comm = new SQLiteCommand(con);
+                comm.CommandText = "SELECT id FROM languages WHERE Name_Eng = @1;";
+                comm.Parameters.AddWithValue("@1", xname);
+                SQLiteDataReader rd = comm.ExecuteReader();
+
+                while (rd.Read())
+                {
+                    id = rd.GetInt32(0);
+                }
+                comm.Dispose();
+                con.Close();
+            }
+            catch (SQLiteException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            return id;
+        }
+
+        public void change_languages(string xname_eng, string xname_nat, int id)
+        {
+            try
+            {
+                SQLiteConnection con = new SQLiteConnection("data source= questionsAnswers.db");
+                con.Open();
+                SQLiteCommand comm = new SQLiteCommand(con);
+                comm.CommandText = "UPDATE languages SET Name_Eng = @1, Name_Native = @2 WHERE id = @3;";
+                comm.Parameters.AddWithValue("@1", xname_eng);
+                comm.Parameters.AddWithValue("@2", xname_nat);
+                comm.Parameters.AddWithValue("@3", id);
+                comm.ExecuteNonQuery();
+                comm.Dispose();
+                con.Close();
+            }
+            catch (SQLiteException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        public void delete_languages(int id)
+        {
+            try
+            {
+                SQLiteConnection con = new SQLiteConnection("data source= questionsAnswers.db");
+                con.Open();
+                SQLiteCommand comm = new SQLiteCommand(con);
+                comm.CommandText = "DELETE FROM languages WHERE id = @1;";
+                comm.Parameters.AddWithValue("@1", id);
+                comm.ExecuteNonQuery();
+                comm.Dispose();
+                con.Close();
+            }
+            catch (SQLiteException ex)
             {
                 MessageBox.Show(ex.ToString());
             }
